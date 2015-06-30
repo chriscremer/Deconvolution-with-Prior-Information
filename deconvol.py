9dc3bbdc12d73c5df3b18b80034ba5a308f17360
chriscremer <chris.a.cremer@gmail.com>
2015-06-30 19:35:13 -0400

    code updated

--- deconvol.py
+++ deconvol.py
@@ -9,8 +9,9 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 #sys.path.insert(0,parentdir) 
 sys.path.insert(0,currentdir+'/simulated_data')
 
-#for many things
 import numpy as np
+
+import itertools
 
 from numpy.linalg import inv
 
@@ -18,8 +19,9 @@ import make_convoluted_data
 
 from sklearn.decomposition import PCA
 from sklearn.decomposition import NMF
+from sklearn.decomposition import fastica
 
-
+import multiprocessing as mp
 
 
 
@@ -48,8 +50,8 @@ def read_data_folder():
 			data.append(samp)
 
 			count += 1
-			if count > 40:
-				break
+			#if count > 40:
+			#	break
 
 	data = np.array(data)
 	print 'real data shape ' + str(data.shape)
@@ -58,8 +60,6 @@ def read_data_folder():
 
 
 def get_possible_Ws(freqs):
-
-	import itertools
 
 	possible_Ws = []
 
@@ -114,71 +114,22 @@ def get_possible_Ws(freqs):
 
 		possible_Ws.append(perms)
 
+	return possible_Ws
 
-def select_w(X, freqs, TZ):
 
-
-	import itertools
+def select_w(X, possible_Ws, TZ):
 
 	W = []
 
-	for i in range(len(freqs)):
-
-		#find permuations and remove the duplicates
-		perms = list(set(itertools.permutations(freqs[i])))
-
-		#now find all combinations of sums of freqs (combine 2)
-		combs = list(itertools.combinations(range(len(freqs[i])), 2))
-		for j in range(len(combs)):
-			#see if this combo has any zeros
-			#if it does skip it
-			skip = 0
-			for index in combs[j]:
-				if freqs[i][index] == 0.0:
-					skip =1 
-					break
-			if skip == 1:
-				continue
-			#combine the freqencies and replace one with a zero
-			new_freq = list(freqs[i])
-			new_freq[combs[j][0]] = new_freq[combs[j][0]] + new_freq[combs[j][1]]
-			new_freq[combs[j][1]] = 0.0
-			new_perms = list(set(itertools.permutations(new_freq)))
-			perms.extend(new_perms)
-		#now find all combinations of sums of freqs (combine 3)
-		combs = list(itertools.combinations(range(len(freqs[i])), 3))
-		for j in range(len(combs)):
-			#see if this combo has any zeros
-			#if it does skip it
-			skip = 0
-			for index in combs[j]:
-				if freqs[i][index] == 0.0:
-					skip =1 
-					break
-			if skip == 1:
-				continue
-			#combine the freqencies and replace one with a zero
-			new_freq = list(freqs[i])
-			new_freq[combs[j][0]] = new_freq[combs[j][0]] + new_freq[combs[j][1]] + new_freq[combs[j][2]]
-			new_freq[combs[j][1]] = 0.0
-			new_freq[combs[j][2]] = 0.0
-			new_perms = list(set(itertools.permutations(new_freq)))
-			perms.extend(new_perms)
-
-		#print len(combs)
-		#print combs
-
-		perms = list(set(perms))
-
-		#print len(perms)
+	for i in range(len(possible_Ws)):
 
 		best_perm = []
 		best_norm = -1
 
-		for perm in perms:
+		for perm in possible_Ws[i]:
 
-			perm = np.array(perm)
-			X_hat = np.dot(TZ.T, perm)
+			perm1 = np.array(perm)
+			X_hat = np.dot(TZ.T, perm1)
 			norm = np.linalg.norm(X[i] - X_hat)
 
 			if norm < best_norm or best_norm == -1:
@@ -191,25 +142,69 @@ def select_w(X, freqs, TZ):
 
 
 
+def doWork(samp_index):
+
+	#print 'Doing sample ' + str(samp_index)
+
+	best_perm = []
+	best_norm = -1
+	for perm in possible_Ws[samp_index]:
+
+		perm1 = np.array(perm)
+		X_hat = np.dot(TZ.T, perm1)
+		norm = np.linalg.norm(X[samp_index] - X_hat)
+
+		if norm < best_norm or best_norm == -1:
+			best_norm = norm
+			best_perm = np.array(perm)
+
+	return best_perm
+
+	
+
+def select_w_parallel():
+
+	try:
+
+		samp_indexes = [i for i in range(len(possible_Ws))]
+		numb_cpus = mp.cpu_count() - 4
+		pool = mp.Pool(processes=numb_cpus)
+
+		W = pool.map(doWork, samp_indexes)
+
+
+		#print 'W ' + str(len(W)) + ' ' + str(len(W[0]))
+
+	except KeyboardInterrupt:
+		print 'keyboard interruption'
+
+	#print np.array(W).shape
+
+	#return np.reshape(np.array(W), (len(W)))
+
+	return np.array(W)
+
 
 if __name__ == "__main__":
 
-
-	real_data = read_data_folder()
+	#real_data = read_data_folder()
 
 	#params: #subpops, #feats, #samps
-	numb_samps = len(real_data)
-	numb_feats = len(real_data[0])
-	numb_subpops = 3
+	#numb_samps = len(real_data)
+	#numb_feats = len(real_data[0])
+	numb_samps = 50
+	numb_feats = 10000
+	numb_subpops = 5
 	samps, freqs, subpops = make_convoluted_data.run_and_return(numb_subpops, numb_feats, numb_samps)
 
+	global X
 	X = samps
 
 	print 'samps shape ' + str(samps.shape)
 	print 'freqs shape ' + str(freqs.shape)
 	print 'subpops shape ' + str(subpops.shape)
 
-	numb_model_subpops= 4
+	numb_model_subpops= 5
 	pca = PCA(n_components=numb_model_subpops)
 	pca.fit(samps.T)
 	#print(pca.explained_variance_ratio_)
@@ -224,6 +219,7 @@ if __name__ == "__main__":
 		#if less than number of model, add zeros
 		while len(freq) < numb_model_subpops:
 			freq.append(0.0)
+		#if  more than number of model,then take largest
 		if len(freq) > numb_model_subpops:
 			sorted_freq = sorted(freq, reverse=True)
 			while len(freq) > numb_model_subpops:
@@ -253,15 +249,21 @@ if __name__ == "__main__":
 	norm = np.linalg.norm(samps - X_hat)
 	print 'Initial norm ' + str(norm)
 
-
+	print 'Finding all weight permutations..'
+	global possible_Ws
 	possible_Ws = get_possible_Ws(freqs)
+	print 'Completed.'
+
+
+	print 'Optimizing model..'
 
 	for i in range(20):
 
 		print 'Iter ' + str(i)
 
 		print 'selecting W'
-		W = select_w(X, freqs, TZ)
+		# W = select_w(X, possible_Ws, TZ)
+		W = select_w_parallel()
 		X_hat = np.dot(W, TZ)
 		norm = np.linalg.norm(X - X_hat)
 		print '         Norm ' + str(norm)
@@ -275,6 +277,78 @@ if __name__ == "__main__":
 		if norm == new_norm:
 			break
 
+	print 
+	#figure out how close the model components are to the actual profiles
+	#and match the components to their profiles so printing makes sense
+	# so for each actual profile, find the row of TZ that is most similar to it
+
+	'''
+	#this list keeps track of which profile correspond to which compoents
+	#also this list is so that I dont assign a profiles to the same components
+	component_order = []
+	for profile_index in range(len(subpops)):
+
+		#what happens when # subpops != #components??
+		best_norm = -1
+		best_component = -1
+
+		for component_index in range(len(TZ)):
+
+			if component_index in component_order:
+				continue
+
+			norm = np.linalg.norm(subpops[profile_index] - TZ[component_index])
+			if norm < best_norm or best_norm == -1:
+				best_norm = norm
+				best_component = component_index
+
+
+		component_order.append(best_component)
+
+		#print how close each component is to each matching profile
+		print 'Profile ' + str(profile_index) + ' norm ' + str(best_norm)
+	'''
+
+	#that is one way of doing it but I dont think its optimal
+	#a better way is too find all permutations and pick the one that minizes the sum of norms
+	possible_component_order = list(set(itertools.permutations(range(len(TZ)))))
+	best_norm_sum = -1
+	best_order = -1
+	for i in range(len(possible_component_order)):
+
+		norm_sum = 0
+		for profile_index in range(len(subpops)):
+
+			norm_sum += np.linalg.norm(subpops[profile_index] - TZ[possible_component_order[i][profile_index]])
+
+		if norm_sum < best_norm_sum or best_norm_sum == -1:
+			best_norm_sum = norm_sum
+			best_order = i
+
+	component_order = possible_component_order[best_order]
+
+	print 
+	#print how close each component is to each matching profile
+	for profile_index in range(len(subpops)):
+
+		print 'Profile ' + str(profile_index) + ' norm ' + str(np.linalg.norm(subpops[profile_index] - TZ[component_order[profile_index]]))
+
+
+
+
+	#print actual frequencies of samples and predicted assignment of frequencies
+	print 'Actual - Predicted'
+	for i in range(len(freqs)):
+
+		#print str(freqs[i]) + '  ' + str(W[i])
+		#print str(freqs[i]) + '  ' + str([W[i][x] for x in component_order])
+
+		#print("%.2f" % a)
+		#myFormattedList = [ '%.2f' % elem for elem in myList ]
+
+		print str(['%.2f' % elem for elem in freqs[i]]) + '  ' + str(['%.2f' % elem for elem in [W[i][x] for x in component_order]])
+
+	#print freqs
 
 	#print W
 
---
+++ simulated_data/make_convoluted_data.py
@@ -0,0 +1,141 @@
+
+import numpy as np
+import random
+
+
+#for now, make samples have 1000 features
+
+
+
+
+def make_subpopulations(numb_of_subpops, numb_of_feats):
+	'''
+	Define possible subpopulations/subtypes profiles
+	'''
+
+	subpops = np.random.rand(numb_of_subpops, numb_of_feats)
+
+	return subpops
+
+
+def subpop_frequencies(numb_samps, numb_of_subpops):
+	'''
+	Define the frequency of each subpop in each sample
+	'''
+
+	#freqs = np.random.rand(numb_samps, numb_of_subpops)
+	#I want some samples to have 0 frequency for some profiles
+	freqs = []
+	for i in range(numb_samps):
+		this_samp = []
+		for j in range(numb_of_subpops):
+			rand = random.random()
+			if rand > 0.5:
+				this_samp.append(0.0)
+			else:
+				this_samp.append(random.random())
+		freqs.append(this_samp)
+	freqs = np.array(freqs)
+
+	#each vector should sum to 1
+	for i in range(len(freqs)):
+		sum_of_row = sum(freqs[i])
+		if sum_of_row == 0.0:
+			#avoid sample with all zeros
+			a = random.randint(0, numb_of_subpops-1)
+			freqs[i][a] = 1.0
+			continue
+		#divide row by sum
+		freqs[i] = freqs[i] / sum_of_row
+
+	
+	return freqs
+
+
+def make_samps(numb_samps, subpops, freqs):
+
+	samps = np.zeros((numb_samps, len(subpops[0])))
+
+
+	for i in range(len(freqs)):
+
+		for j in range(len(freqs[0])):
+
+			# print 'samps1 shape ' + str(samps[i].shape)
+			# print 'freqs1 shape ' + str(freqs[i][j].shape)
+			# print 'subpops1 shape ' + str(subpops[j].shape)
+			samps[i] = samps[i] + freqs[i][j]*subpops[j]
+
+	
+	#add some noise, divide to make it smaller
+	noise = np.random.rand(numb_samps, len(subpops[0])) / 100.0
+	samps = samps + noise
+	#need to make sure that no cell is negative
+	for i in range(len(samps)):
+		for j in range(len(samps[0])):
+			if samps[i][j] < 0.0:
+				samps[i][j] = 0.0
+
+
+
+	return samps
+
+
+
+
+def make_csv(data):
+	'''
+	Writes the data to a csv file
+	'''
+
+	f = open('simulated_data_' + str(numb_samps) + '_' + str(numb_of_feats) + '.csv', 'w')
+
+	for samp in range(len(data)):
+		for feature in range(len(data[samp])):
+			f.write(str(data[samp][feature]) + ', ')
+		f.write('\n')
+	f.close()
+
+
+	print 'File has been made.'
+
+
+
+def run_and_return(numb_of_subpops, numb_of_feats, numb_samps):
+	'''
+	Run the methods to create the data and return it
+	'''
+
+	subpops = make_subpopulations(numb_of_subpops, numb_of_feats)
+	#print 'subpops shape ' + str(subpops.shape)
+	freqs = subpop_frequencies(numb_samps, numb_of_subpops)
+	#print 'freqs shape ' + str(freqs.shape)
+	samps = make_samps(numb_samps, subpops, freqs)
+	#print 'samps shape ' + str(samps.shape)
+
+	return samps, freqs, subpops
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	numb_of_subpops = 10
+	numb_of_feats = 100
+	numb_samps = 50
+
+	# subpops = make_subpopulations(numb_of_subpops, numb_of_feats)
+	# print 'subpops shape ' + str(subpops.shape)
+	# freqs = subpop_frequencies(numb_samps, numb_of_subpops)
+	# print 'freqs shape ' + str(freqs.shape)
+	# samps = make_samps(numb_samps, subpops, freqs)
+	# print 'samps shape ' + str(samps.shape)
+	
+	#make_csv(samps)
+	#run_and_return(numb_of_subpops, numb_of_feats, numb_samps)
+
